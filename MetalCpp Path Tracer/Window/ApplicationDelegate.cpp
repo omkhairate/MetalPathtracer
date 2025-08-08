e0de99cc33e2c2d9dc2bd5b665565894864f5af9
Om Khairate <72100111+omkhairate@users.noreply.github.com>
2025-08-08 12:05:09 +0200

    Use custom view for input and rebuild acceleration structures

--- MetalCpp Path Tracer/Window/ApplicationDelegate.cpp
+++ MetalCpp Path Tracer/Window/ApplicationDelegate.cpp
@@ -2,59 +2,63 @@
 
 using namespace MetalCppPathTracer;
 
-ApplicationDelegate::~ApplicationDelegate()
-{
-    if (_pMtkView) _pMtkView->release();
-    if (_pWindow) _pWindow->release();
-    if (_pDevice) _pDevice->release();
-    if (_pViewDelegate) delete _pViewDelegate;
+ApplicationDelegate::~ApplicationDelegate() {
+  if (_pMtkView)
+    _pMtkView->release();
+  if (_pWindow)
+    _pWindow->release();
+  if (_pDevice)
+    _pDevice->release();
+  if (_pViewDelegate)
+    delete _pViewDelegate;
 }
 
-void ApplicationDelegate::applicationWillFinishLaunching(NS::Notification* pNotification)
-{
-    NS::Application* pApp = reinterpret_cast<NS::Application*>(pNotification->object());
-    pApp->setActivationPolicy(NS::ActivationPolicy::ActivationPolicyRegular);
+void ApplicationDelegate::applicationWillFinishLaunching(
+    NS::Notification *pNotification) {
+  NS::Application *pApp =
+      reinterpret_cast<NS::Application *>(pNotification->object());
+  pApp->setActivationPolicy(NS::ActivationPolicy::ActivationPolicyRegular);
 }
 
-void ApplicationDelegate::applicationDidFinishLaunching(NS::Notification* pNotification)
-{
-    if(_initialized)
-        return;
-    _initialized = true;
-    CGRect frame = {{100.0, 100.0}, {1280.0, 720.0}};
+void ApplicationDelegate::applicationDidFinishLaunching(
+    NS::Notification *pNotification) {
+  if (_initialized)
+    return;
+  _initialized = true;
+  CGRect frame = {{100.0, 100.0}, {1280.0, 720.0}};
 
-    _pWindow = NS::Window::alloc()->init(
-        frame,
-        NS::WindowStyleMaskClosable | NS::WindowStyleMaskTitled | NS::WindowStyleMaskResizable,
-        NS::BackingStoreBuffered,
-        false
-    );
+  _pWindow = NS::Window::alloc()->init(frame,
+                                       NS::WindowStyleMaskClosable |
+                                           NS::WindowStyleMaskTitled |
+                                           NS::WindowStyleMaskResizable,
+                                       NS::BackingStoreBuffered, false);
 
-    _pDevice = MTL::CreateSystemDefaultDevice();
+  _pDevice = MTL::CreateSystemDefaultDevice();
 
-    _pMtkView = MTK::View::alloc()->init(frame, _pDevice);
-    _pMtkView->setDevice(_pDevice);
-    _pMtkView->setColorPixelFormat(MTL::PixelFormat::PixelFormatRGBA16Float);
-    _pMtkView->setClearColor(MTL::ClearColor::Make(0.0, 0.0, 0.0, 1.0));
-    _pMtkView->setPreferredFramesPerSecond(60);
-    _pMtkView->setEnableSetNeedsDisplay(false);
-    _pMtkView->setPaused(false);
+  _pMtkView = _controllerView.get(frame);
+  _pMtkView->setDevice(_pDevice);
+  _pMtkView->setColorPixelFormat(MTL::PixelFormat::PixelFormatRGBA16Float);
+  _pMtkView->setClearColor(MTL::ClearColor::Make(0.0, 0.0, 0.0, 1.0));
+  _pMtkView->setPreferredFramesPerSecond(60);
+  _pMtkView->setEnableSetNeedsDisplay(false);
+  _pMtkView->setPaused(false);
 
-    _pViewDelegate = new ViewDelegate(_pDevice);
-    _pMtkView->setDelegate(_pViewDelegate);
+  _pViewDelegate = new ViewDelegate(_pDevice);
+  _pMtkView->setDelegate(_pViewDelegate);
 
-    _pWindow->setContentView(_pMtkView);
-    _pWindow->setTitle(NS::String::string("MetalCpp Path Tracer", NS::StringEncoding::UTF8StringEncoding));
-    _pWindow->makeKeyAndOrderFront(nullptr);
+  _pWindow->setContentView(_pMtkView);
+  _pWindow->setTitle(NS::String::string(
+      "MetalCpp Path Tracer", NS::StringEncoding::UTF8StringEncoding));
+  _pWindow->makeKeyAndOrderFront(nullptr);
 
-    NS::Application* pApp = reinterpret_cast<NS::Application*>(pNotification->object());
-    pApp->activateIgnoringOtherApps(true);
+  NS::Application *pApp =
+      reinterpret_cast<NS::Application *>(pNotification->object());
+  pApp->activateIgnoringOtherApps(true);
 }
 
-bool ApplicationDelegate::applicationShouldTerminateAfterLastWindowClosed(NS::Application* /*pSender*/)
-{
-    return true;
+bool ApplicationDelegate::applicationShouldTerminateAfterLastWindowClosed(
+    NS::Application * /*pSender*/) {
+  return true;
 }
 
-
-//hehehehe
+// hehehehe
--- MetalCpp Path Tracer/Window/ApplicationDelegate.h
+++ MetalCpp Path Tracer/Window/ApplicationDelegate.h
@@ -1,34 +1,37 @@
 #ifndef APPLICATION_DELEGATE_H
 #define APPLICATION_DELEGATE_H
 
-#include <Metal/Metal.hpp>
-#include <MetalKit/MetalKit.hpp>
 #include <AppKit/AppKit.hpp>
 #include <Foundation/Foundation.hpp>
+#include <Metal/Metal.hpp>
+#include <MetalKit/MetalKit.hpp>
 
+#include "ControllerView.hpp"
 #include "ViewDelegate.h"
 
-namespace MetalCppPathTracer
-{
+namespace MetalCppPathTracer {
 
-class ApplicationDelegate : public NS::ApplicationDelegate
-{
-    public:
-        ~ApplicationDelegate();
-    
-        ApplicationDelegate() = default;
-        virtual void applicationWillFinishLaunching( NS::Notification* pNotification ) override;
-        virtual void applicationDidFinishLaunching( NS::Notification* pNotification ) override;
-        virtual bool applicationShouldTerminateAfterLastWindowClosed( NS::Application* pSender ) override;
+class ApplicationDelegate : public NS::ApplicationDelegate {
+public:
+  ~ApplicationDelegate();
 
-    private:
-        NS::Window* _pWindow;
-        MTK::View* _pMtkView;
-        MTL::Device* _pDevice;
-        ViewDelegate* _pViewDelegate = nullptr;
-        bool _initialized = false;
+  ApplicationDelegate() = default;
+  virtual void
+  applicationWillFinishLaunching(NS::Notification *pNotification) override;
+  virtual void
+  applicationDidFinishLaunching(NS::Notification *pNotification) override;
+  virtual bool applicationShouldTerminateAfterLastWindowClosed(
+      NS::Application *pSender) override;
+
+private:
+  NS::Window *_pWindow;
+  MTK::View *_pMtkView;
+  MTL::Device *_pDevice;
+  ViewDelegate *_pViewDelegate = nullptr;
+  ControllerView _controllerView;
+  bool _initialized = false;
 };
 
-};
+}; // namespace MetalCppPathTracer
 
-#endif  //  APPLICATION_DELEGATE_H
+#endif //  APPLICATION_DELEGATE_H
