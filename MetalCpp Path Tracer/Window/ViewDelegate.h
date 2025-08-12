1a9c73068de39e585f396e04a89c1bc84a3f06c3
Om Khairate <72100111+omkhairate@users.noreply.github.com>
2025-08-12 18:04:18 +0200

    Dump per-frame acceleration structure

--- MetalCpp Path Tracer/Window/ViewDelegate.h
+++ MetalCpp Path Tracer/Window/ViewDelegate.h
@@ -5,6 +5,7 @@
 #include <MetalKit/MetalKit.hpp>
 #include <cstddef>
 #include <chrono>
+#include <string>
 
 #include "Renderer.h"
 
@@ -24,6 +25,7 @@ class ViewDelegate : public MTK::ViewDelegate
     std::size_t _frameCount = 0;
     std::size_t _maxFrames = 0;
     std::chrono::steady_clock::time_point _lastTime;
+    std::string _dumpPath;
 };
 
 };
